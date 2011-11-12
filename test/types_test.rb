d80e17a4c4a3bff3f6978ae8693bb4ac44d518a9
Michael Guterl <michael@diminishing.org>
2011-11-12 10:12:49 -0500

    properly handle encoding booleans

--- test/types_test.rb
+++ test/types_test.rb
@@ -15,4 +15,18 @@ class SerializedAttributesTypesTest < ActiveSupport::TestCase
     assert_equal nil, type.encode("")
   end
 
+  test "boolean type handles strings that look like booleans" do
+    type = SerializedAttributes::Boolean.new
+
+    assert_equal 0, type.encode("false")
+    assert_equal 1,  type.encode("true")
+  end
+
+  test "boolean type encodes booleans properly" do
+    type = SerializedAttributes::Boolean.new
+
+    assert_equal 0, type.encode(false)
+    assert_equal 1, type.encode(true)
+  end
+
 end
