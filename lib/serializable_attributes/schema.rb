16e113553f568d0758ec5ed9a829034335625f8c
Ryan Tomayko <rtomayko@gmail.com>
2013-12-03 11:42:35 -0800

    Avoid writing blob field if no attributes change

--- lib/serializable_attributes/schema.rb
+++ lib/serializable_attributes/schema.rb
@@ -130,7 +130,9 @@ module SerializableAttributes
 
       @model.before_save do |r|
         schema = r.class.send("#{data_field}_schema")
-        r.send("#{blob_field}=", schema.encode(r.send(data_field)))
+        if r.send(blob_field).nil? || !r.send(changed_ivar).empty?
+          r.send("#{blob_field}=", schema.encode(r.send(data_field)))
+        end
       end
     end
 
